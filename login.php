3a7fe3aba048bf023c3f44d291cee2b9a447d030
FabricioAlama2001 <88296370+FabricioAlama2001@users.noreply.github.com>
2023-08-31 00:07:10 -0500

    Login, Registrate

--- login.php
+++ login.php
@@ -23,21 +23,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
             exit;
         }
     }
-
-    $loginError = "Credenciales incorrectas";
+    $loginError = "<span class='error-message'>Usuario o contraseña incorrecta</span>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap">
+    <link rel="stylesheet" href="./css/main.css">
+    <style>
+        .wrapper2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: var(--clr-main);
+}
+.error-message {
+        color: red;
+        font-weight: bold;
+    }
+    </style>
+</head>
+<body>
+    <div class="wrapper2">
+        <aside> </aside>
+        <main>
+            <h1 class="titulo-principal text-center">Stock X</h1>
+            <form method="POST" action="login.php" class="formulario-login">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Correo electrónico</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese su correo electrónico" required>
+                </div>
+            <div class="mb-3">
+             <label for="password" class="form-label">Contraseña</label>
+             <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese su contraseña" required>
+                </div>
+                <button type="submit" name="login" class="producto-agregar boton-login">Iniciar sesión</button>
+            </form>
+            <a href="registrate.php" class="btn producto-agregar">Registrarse</a>
+            <?php
+            if (isset($loginError)) {
+                echo "<p class='error'>$loginError</p>";
+            }
+            ?>
+        </main>
+    </div>
+</body>
+</html>
 
-<form method="POST" action="login.php">
-    <input type="email" name="email" placeholder="Correo electrónico" required>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <button type="submit" name="login">Iniciar sesión</button>
-</form>
-
-<a href="registrate.php">Registrarse</a>
 
 <?php
 if (isset($loginError)) {
     echo "<p>$loginError</p>";
 }
 ?>
+
+<script>
+    <?php if (isset($loginError)) : ?>
+        $(document).ready(function () {
+            $('#errorModal').modal('show');
+            $('.error-message').css('color', 'red'); 
+        });
+    <?php endif; ?>
+</script>
--- registrate.php
+++ registrate.php
@@ -1,4 +1,6 @@
 <?php
+$registrationMessage = ""; // Inicializa el mensaje de registro
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
     $email = $_POST["email"];
     $firstName = $_POST["first_name"];
@@ -11,18 +13,83 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
         file_put_contents("users.txt", "");
     }
 
-    if (file_put_contents("users.txt", $userRecord, FILE_APPEND)) {
-        echo "Registro exitoso. ¡Ahora puedes iniciar sesión!";
+    $lines = file("users.txt", FILE_IGNORE_NEW_LINES);
+
+    $emailExists = false;
+    foreach ($lines as $line) {
+        list($storedEmail, $storedFirstName, $storedLastName, $hashedPassword) = explode(":", $line);
+        if ($email === $storedEmail) {
+            $emailExists = true;
+            break;
+        }
+    }
+
+    if ($emailExists) {
+        $registrationMessage = "El correo electrónico ya está registrado.";
     } else {
-        echo "Error al registrar el usuario.";
+        if (file_put_contents("users.txt", $userRecord, FILE_APPEND)) {
+            $registrationMessage = "Registro exitoso. ¡Ahora puedes iniciar sesión!";
+        } else {
+            $registrationMessage = "Error al registrar el usuario.";
+        }
     }
 }
 ?>
-
-<form method="POST" action="registrate.php">
-    <input type="email" name="email" placeholder="Correo electrónico" required>
-    <input type="text" name="first_name" placeholder="Nombre" required>
-    <input type="text" name="last_name" placeholder="Apellido" required>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <button type="submit" name="register">Registrarse</button>
-</form>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap">
+    <link rel="stylesheet" href="./css/main.css">
+    <style>
+        .wrapper2 {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: var(--clr-main);
+        }
+        .error-message {
+            color: red;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper2">
+        <aside> </aside>
+        <main>
+            <h1 class="titulo-principal text-center">Stock X</h1>
+            <form method="POST" action="registrate.php" class="formulario-login">
+                 <div class="mb-3">
+                    <label for="email" class="form-label">Correo electrónico</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese su correo electrónico" required>
+                </div>
+                <div class="mb-3">
+                    <label for="first_name" class="form-label">Nombre</label>
+                    <input type="text" name="first_name" class="form-control" id="first_name" placeholder="Ingrese su nombre" required>
+                </div>
+                <div class="mb-3">
+                    <label for="last_name" class="form-label">Apellido</label>
+                    <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Ingrese su apellido" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Contraseña</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese su contraseña" required>
+                </div>
+                <button type="submit" name="register" class="producto-agregar boton-login">Registrarse</button>
+            </form>
+            <a href="login.php" class="btn producto-agregar">Iniciar Sesión</a>
+            <?php
+            if (!empty($registrationMessage)) {
+                echo "<p class='registration-message'>$registrationMessage</p>";
+            }
+            ?>
+        </main>
+    </div>
+</body>
+</html>
